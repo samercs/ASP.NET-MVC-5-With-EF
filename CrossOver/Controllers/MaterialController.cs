76daa77a614d9907af1dfefdded7525525e38470
samer al-khatib <samer_mail_2006@yahoo.com>
2016-08-24 23:22:32 +0300

    add material design template

---
+++ CrossOver/Controllers/MaterialController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrossOver.Controllers
+{
+    public class MaterialController : Controller
+    {
+        // GET: Material
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Template2()
+        {
+            return View();
+        }
+
+        
+    }
+}
